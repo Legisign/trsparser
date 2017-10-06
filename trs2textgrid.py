20c7000fa26e0cb108288ef2be6164895a83b50a
Legisign <32565976+Legisign@users.noreply.github.com>
2017-10-06 11:48:52 +0300

    Add files via upload

---
+++ trs2textgrid.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+'''trs2textgrid.py -- Transcriber to Praat interval tier converter
+
+  A simple Transcriber .trs to Praat .TextGrid format converter.
+  Transcriber files can contain only one episode and one section.
+  Turns inside that section are converted into Praat interval tiers,
+  and timepoints/data inside turns into intervals in that tier.
+
+  Tommi Nieminen <software@legisign.org> 2017.
+  Licensed under GNU General Public License (GPL) version 3.0 or newer.
+
+  Usage:
+    trs2textgrid trsfile...
+
+  Error handling is rudimentary. In read/parse errors, the file is
+  simply omitted. Write errors cause the script to stop (since it is
+  to be expected that if you cannot write ONE file, you may not be
+  able to write the rest of them!).
+
+  2017-10-05    0.0.1   Development started. (TN)
+  2017-10-05    0.0.2   Changed structure. (TN)
+
+'''
+
+import sys
+import os.path
+import trsparser
+
+version = '0.0.2'
+
+def write_praat(data, filename):
+    '''Write Transcriber data as Praat TextGrid'''
+    # Only the first section of the first episode is read and converted
+    section = data.episodes[0][0]
+    with open(filename, 'w') as out:
+        buff = ['File type = "ooTextFile"',
+                'Object class = "TextGrid"',
+                '',
+                'xmin = {}\n'.format(section.beg),
+                'xmax = {}\n'.format(section.end),
+                'tiers? <exists>',
+                'size = {}\n'.format(len(section)),
+                'item []:']
+        out.writelines([line + '\n' for line in buff])
+
+        for turnid, turn in enumerate(section):
+            indent = ' ' * 4
+            out.write('{}item[{}]\n'.format(indent, turnid + 1))
+            indent = ' ' * 8
+            buff = ['class = "IntervalTier"',
+                    'name = "{}"'.format(turnid + 1),
+                    'xmin = {}'.format(turn.beg),
+                    'xmax = {}'.format(turn.end),
+                    'intervals: size = {}'.format(len(turn))]
+            out.writelines(['{}{}\n'.format(indent, line) \
+                            for line in buff])
+            for chunkid, chunk in enumerate(turn):
+                buff = ['intervals [{}]:'.format(chunkid),
+                        'xmin = {}'.format(chunk.beg),
+                        'xmax = {}'.format(chunk.end),
+                        'text = "{}"'.format(chunk.text.replace('"', '""'))]
+                out.writelines(['{}{}\n'.format(indent, line) \
+                                for line in buff])
+
+if not sys.argv[1:]:
+    print('Käyttö: trs2textgrid trs-tiedosto...', file=sys.stderr)
+    sys.exit(0)
+
+for name in sys.argv[1:]:
+    try:
+        trs = trsparser.TransObject(name)
+    except (IOError, ValueError, xml.parsers.expat.ExpatError):
+        print('VIRHE: tiedostonlukuvirhe: "{}"'.format(name), file=sys.stderr)
+        continue
+
+    # This is important!
+    trs.to_intervals()
+
+    if len(trs.episodes) != 1 or len(trs.episodes[0]) != 1:
+        print('VIRHE: Skripti hallitsee vain yksiepisodiset .trs-tiedostot',
+              file=sys.stderr)
+        continue
+
+    textgrid = os.path.splitext(name)[0] + '.TextGrid'
+    try:
+        write_praat(trs, textgrid)
+    except (PermissionError, IOError):
+        print('VIRHE: Tiedostoa ei voi kirjoittaa: "{}"'.format(textgrid),
+              file=sys.stderr)
+        sys.exit(1)
---
+++ trsparser.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+'''trsparser.py -- Transcriber parser
+
+  A parser for Transcriber .trs file format.
+
+  Tommi Nieminen <software@legisign.org> 2017.
+  Licensed under GNU General Public License (GPL) version 3.0 or newer.
+
+  2017-10-05    0.0.1   Development started.
+  2017-10-05    0.0.2   Full classes instead of named tuples to ensure easier
+                        iteration.
+  2017-10-05    0.0.3   Added TransObject.to_intervals() method.
+  2017-10-05    0.0.4   Added ParseError.
+
+  Error handling is mostly left for the caller. Possible exceptions include
+  * IOError and its subcategories when input file cannot be read,
+  * ParseError for Transcriber-specific parse errors, and
+  * xml.parsers.expat.ExpatError for general XML parse errors.
+
+  BUG: Instead of turn’s end time, to_intervals() should use the time in the
+  last (empty) <Sync> node. This is tricky to catch, however, since times are
+  collected only when there IS character data.
+
+  POSSIBLE BUG: Assumes Latin-1 (ISO 8859-1) character coding in the .trs
+  file. This may have changed in later Transcriber versions.
+
+'''
+
+import codecs
+import xml.parsers.expat
+
+version = '0.0.4'
+
+# Classes
+
+class ParseError(Exception):
+    '''Transcriber-specific parse error.'''
+    pass
+
+class Chunk(object):
+    '''A chunk is the smallest-level object: text synced with time.'''
+    def __init__(self, beg=0, end=0, text=None):
+        self.beg = beg
+        self.end = end
+        self.text = text
+
+class Turn(list):
+    '''A turn is a list of chunks.'''
+    def __init__(self, beg=0, end=0, chunks=None):
+        self.beg = beg
+        self.end = end
+        if not chunks:
+            chunks = []
+        super().__init__(chunks)
+
+class Section(list):
+    '''A section is a list of turns.'''
+    def __init__(self, category=None, beg=0, end=0, turns=None):
+        self.category = category
+        self.beg = beg
+        self.end = end
+        if not turns:
+            turns = []
+        super().__init__(turns)
+
+class Episode(list):
+    '''An episode is a list of sections.'''
+    def __init__(self, sections=None):
+        if not sections:
+            sections = []
+        super().__init__(sections)
+
+class TransObject(object):
+    '''Transcriber object: the largest-level object.
+
+    Topmost is a list of Episodes, each of which contains one or more
+    Sections, each of which contains one or more Turns, each of which
+    contains one or more Chunks (transcribed speech data with timepoints).
+
+    '''
+    def __init__(self, filename):
+        # XML Parser
+        self._parser = xml.parsers.expat.ParserCreate()
+        self._parser.StartElementHandler = self._start_element
+        self._parser.EndElementHandler = self._end_element
+        self._parser.CharacterDataHandler = self._char_data
+
+        # Current episode, section, and turn
+        self._curr_episode = None
+        self._curr_section = None
+        self._curr_turn = None
+
+        # Sync time
+        self._caret = None
+
+        # Public: when using this module, use only these variables
+        # and the to_intervals() and read() methods
+        self.filename = filename
+        self.episodes = []
+
+        if self.filename:
+            self.read()
+
+    def _start_element(self, element, attrs):
+        '''Private method: handle XML start elements.'''
+        if element == 'Episode':
+            self._curr_episode = Episode()
+        elif element == 'Section':
+            cat = attrs['type']
+            try:
+                beg = float(attrs['startTime'])
+                end = float(attrs['endTime'])
+            except ValueError:
+                raise ParseError
+            self._curr_section = Section(category=cat, beg=beg, end=end)
+        elif element == 'Turn':
+            try:
+                beg=float(attrs['startTime'])
+                end=float(attrs['endTime'])
+            except ValueError:
+                raise ParseError
+            self._curr_turn = Turn(beg=beg, end=end)
+        elif element == 'Sync':
+            try:
+                self._caret = float(attrs['time'])
+            except ValueError:
+                raise ParseError
+
+    def _end_element(self, element):
+        '''Private method: handle XML end elements.'''
+        if element == 'Episode':
+            self.episodes.append(self._curr_episode)
+            self._curr_episode = None
+        elif element == 'Section':
+            self._curr_episode.append(self._curr_section)
+            self._curr_section = None
+        elif element == 'Turn':
+            self._curr_section.append(self._curr_turn)
+            self._curr_turn = None
+
+    def _char_data(self, data):
+        '''Private method: handle character data.'''
+        data = data.strip()
+        if data:
+            self._curr_turn.append(Chunk(beg=self._caret, text=data))
+            self._caret = None
+
+    def to_intervals(self):
+        '''Insert end times for each chunk.
+
+        This is to ensure Praat interval layer compatibility.
+        '''
+        for episode in self.episodes:
+            for section in episode:
+                for turn in section:
+                    for pred, succ in zip(turn[0:], turn[1:]):
+                        pred.end = succ.beg
+                    succ.end = turn.end
+
+    def read(self, filename=None):
+        '''Read and parse a .trs file.'''
+        if filename:
+            self.filename = filename
+        if self.filename:
+            with codecs.open(self.filename, 'r', encoding='iso8859-1') as infile:
+                self._parser.Parse(''.join(infile.readlines()))
+
+if __name__ == '__main__':
+    import sys
+
+    # A simple test run if run as a script
+    for arg in sys.argv[1:]:
+        try:
+            trs = TransObject(arg)
+        except (FileNotFoundError, PermissionError, IOError):
+            print('I/O error: {}'.format(arg), file=sys.stderr)
+            continue
+        except (ParseError, xml.parsers.expat.ExpatError):
+            print('Parse error: {}'.format(arg), file=sys.stderr)
+            continue
+        for episode in trs.episodes:
+            for section in episode:
+                print('section type={} @ {} --> {}'.format(section.category,
+                                                           section.beg,
+                                                           section.end))
+                for turn in section:
+                    print('turn @ {} --> {}'.format(turn.beg, turn.end))
+                    for chunk in turn:
+                        print('@ {} --> {} = "{}"'.format(chunk.beg,
+                                                          chunk.end,
+                                                          chunk.text[:50]))
